f08b4deaad0d9ea0ae24a19b1782f67737f68f14
Haru <140901901+haru-r2h@users.noreply.github.com>
2023-08-07 00:43:49 -0300

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,24 +1,42 @@
-(function() {
-    var start = new Date;
-    start.setHours(18, 0, 0); // 6pm
-  
-    function pad(num) {
-      return ("0" + parseInt(num)).slice(-2);
+(function () {
+  var start = new Date;
+  start.setHours(18, 0, 0); // 6pm
+
+  function pad(num) {
+    return ("0" + parseInt(num)).slice(-2);
+  }
+
+  function timer() {
+    var now = new Date;
+    if (now > start) { // too late, go to tomorrow
+      start.setDate(start.getDate() + 1);
     }
-  
-    function timer() {
-      var now = new Date;
-      if (now > start) { // too late, go to tomorrow
-        start.setDate(start.getDate() + 1);
-      }
-      var remain = ((start - now) / 1000);
-      var hh = pad((remain / 60 / 60) % 60);
-      var mm = pad((remain / 60) % 60);
-      var ss = pad(remain % 60);
-      document.getElementById('time').innerHTML =
-        hh + ":" + mm + ":" + ss;
-      setTimeout(timer, 1000);
-    }
-  
-    document.addEventListener('DOMContentLoaded', timer);
-  })();
+    var remain = ((start - now) / 1000);
+    var hh = pad((remain / 60 / 60) % 60);
+    var mm = pad((remain / 60) % 60);
+    var ss = pad(remain % 60);
+    document.getElementById('time').innerHTML =
+      hh + ":" + mm + ":" + ss;
+    setTimeout(timer, 1000);
+  }
+
+  document.addEventListener('DOMContentLoaded', timer);
+})();
+
+function toggleTheme() {
+  var element = document.body;
+  var themeIcon = document.getElementById("themeIcon");
+  var themeText = document.getElementById("themeText");
+
+  if (element.classList.contains("lightmode")) {
+    element.classList.remove("lightmode");
+    element.classList.add("darkmode");
+    themeIcon.textContent = "🌙";
+    themeText.textContent = "Daylight Mode";
+  } else {
+    element.classList.remove("darkmode");
+    element.classList.add("lightmode");
+    themeIcon.textContent = "🌞";
+    themeText.textContent = "Midnight Mode";
+  }
+}
